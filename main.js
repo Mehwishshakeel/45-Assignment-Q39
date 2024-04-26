f3228a9a066155af8aa9c34acdfdd40af6d8d24d
Mehwish Shakeel <160312242+Mehwishshakeel@users.noreply.github.com>
2024-04-26 14:16:06 +0500

    Return Statement

---
+++ main.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// creating a function
+function city_country(Country, City) {
+    return `${Country} , ${City}`;
+}
+// calling a function abd print the return value
+console.log(city_country("pakistan", "Karachi"));
+console.log(city_country("Canada", "Toronto"));
+console.log(city_country("Germany", "Berlin"));
---
+++ main.ts
@@ -0,0 +1,10 @@
+// creating a function
+function city_country(Country:string , City:string) :string {
+          return `${Country} , ${City}`;
+          
+}
+// calling a function abd print the return value
+console.log(city_country("pakistan" ,"Karachi"));
+console.log(city_country("Canada","Toronto"));
+console.log(city_country("Germany","Berlin"));
+
